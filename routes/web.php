32ff024fe19d354f0e5491148dada2bc8f596198
Pozz Rith <pozzr890@gmail.com>
2024-08-29 16:46:37 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/index', function(){
-    return view('index');
-})->name('index');
-Route::resource('user' , UserController::class);
+//Route::group(['middleware' => ['auth']] , function (){
+    Route::get('/index', function(){
+        return view('index');
+    })->name('index');
+    Route::resource('user' , UserController::class);
+//});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
